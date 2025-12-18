5df310822f719393993e796e8121fade29e3dc72
rysvk <shouvikroy@outlook.com>
2025-12-19 00:59:34 +0530

    2015 Day 2

---
+++ 2015/Day 2/Part 2/soln.py
@@ -0,0 +1,6 @@
+with open('input.txt', 'r') as file:
+    data = file.readlines() 
+lines = [line.split("x") for line in data] 
+sides = [sorted([int(num) for num in line]) for line in lines]
+print(sum(2*(sides[i][0]+sides[i][1]) + sides[i][0]*sides[i][1]*sides[i][2] for i in range(len(sides))))
+
